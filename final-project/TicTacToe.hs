d5c2d0a3a497a7cc7280a6e60f4dcfd893f1e115
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-29 22:14:33 +0000

    Reordered functions

--- final-project/TicTacToe.hs
+++ final-project/TicTacToe.hs
@@ -3,52 +3,6 @@ import System.IO (stdout, hFlush)
 import Data.List (intercalate, intersperse)
 import Data.List.Split (chunksOf)
 
-showIntro :: IO ()
-showIntro = do
-    putStrLn "---------------------------------"
-    putStrLn "Welcome to Tic Tac Toe Haskell!!"
-    putStrLn "---------------------------------\n\n"
-
-gameMenu :: IO ()
-gameMenu = do
-    putStrLn "--------- Game Menu ------------"
-    choice <- getListInput ["One Player", "Two player", "Leave Game"]
-
-    case choice of
-        1   -> do
-            putStrLn "\n-------------- One Player Game ---------------"
-            putStrLn "Enter player name:"
-            name <- getStringInput
-
-            putStrLn "Choose difficulty:"
-            difficulty <- getListInput ["Easy"]
-
-            let human = Player name (humanPlayer name)
-            let computer = Player "Computer" $
-                            case difficulty of
-                                1   ->  easyComputer
-                                _   -> undefined
-
-            playGame human computer
-            gameMenu
-
-        2   -> do
-            putStrLn "\n-------------- Two Player Game ---------------"
-            putStrLn "\nEnter player one's name:"
-            firstName <- getStringInput
-
-            putStrLn "\nEnter player two's name:"
-            secondName <- getStringInput
-
-            let player1 = Player firstName (humanPlayer firstName)
-                player2 = Player secondName (humanPlayer secondName)
-
-            playGame player1 player2
-            gameMenu
-
-        _   -> putStrLn "Thanks for playing!!"
-
-
 type Grid = [Bool]
 type Move = Int
 
@@ -59,56 +13,11 @@ data Player = Player {
         getMove :: MoveCalculation
     }
 
-
 emptyGrid :: Grid
 emptyGrid = replicate 9 False
 
 countGrid :: Grid -> Int
 countGrid = sum . map (\x -> if x then 1 else 0)
-
-easyComputer :: MoveCalculation
-easyComputer fGrid sGrid = return $ head $ dropWhile ((combined !!) . (pred)) moveOrders
-                        where combined = zipWith (||) fGrid sGrid
-                              moveOrders = [ 5, 1, 3, 7, 9, 2, 4, 6, 8] :: [Int]
-
-hardComputer :: MoveCalculation
-hardComputer fGrid sGrid = undefined
-
-humanPlayer :: String -> MoveCalculation
-humanPlayer name fGrid sGrid = do
-    putStrLn $ "\n" ++ name ++ "'s turn:"
-    putStrLn "Choose position (1-9):"
-
-    let validNum n = all ($ (n - 1)) [ (>= 0), (< 9) , not . (fGrid !!), not . (sGrid !!)]
-
-    repeatUntil validNum
-                (putStrLn "Try again - invalid number")
-                getIntInput
-
-showGrids :: Grid -> Grid -> String
-showGrids = intercalate "------\n" .
-            map ((++ "\n") . intersperse '|') .
-            chunksOf 3 `applyTwice`
-            zipWith (\a b -> if a then 'X' else if b then 'O' else '_')
-
-playGame :: Player -> Player -> IO ()
-playGame first second = iterateGame first second emptyGrid emptyGrid
-
-iterateGame :: Player -> Player -> Grid -> Grid -> IO ()
-iterateGame first second fGrid sGrid = do
-    putStrLn "\nCurrent Grid:"
-    putStrLn $ showGrids fGrid sGrid
-
-    firstMove <- getMove first fGrid sGrid
-    let fGrid' = applyMove (firstMove - 1) fGrid
-
-    if checkWin fGrid' then
-        putStrLn $ "\n" ++ getName first ++ " wins!!"
-    else
-        if countGrid fGrid' + countGrid sGrid == 9 then
-            putStrLn $ "\nGame Over - draw"
-        else
-            iterateGame second first sGrid fGrid'
 
 applyMove :: Move -> Grid -> Grid
 applyMove 0 (_:xs) = True:xs
@@ -169,6 +78,95 @@ winningCombinations =
 
 checkWin :: Grid -> Bool
 checkWin grid = any (\win -> win == zipWith (&&) grid win) winningCombinations
+
+easyComputer :: MoveCalculation
+easyComputer fGrid sGrid = return $ head $ dropWhile ((combined !!) . (pred)) moveOrders
+                        where combined = zipWith (||) fGrid sGrid
+                              moveOrders = [ 5, 1, 3, 7, 9, 2, 4, 6, 8] :: [Int]
+
+hardComputer :: MoveCalculation
+hardComputer fGrid sGrid = undefined
+
+humanPlayer :: String -> MoveCalculation
+humanPlayer name fGrid sGrid = do
+    putStrLn $ "\n" ++ name ++ "'s turn:"
+    putStrLn "Choose position (1-9):"
+
+    let validNum n = all ($ (n - 1)) [ (>= 0), (< 9) , not . (fGrid !!), not . (sGrid !!)]
+
+    repeatUntil validNum
+                (putStrLn "Try again - invalid number")
+                getIntInput
+
+showGrids :: Grid -> Grid -> String
+showGrids = intercalate "------\n" .
+            map ((++ "\n") . intersperse '|') .
+            chunksOf 3 `applyTwice`
+            zipWith (\a b -> if a then 'X' else if b then 'O' else '_')
+
+playGame :: Player -> Player -> IO ()
+playGame first second = iterateGame first second emptyGrid emptyGrid
+
+iterateGame :: Player -> Player -> Grid -> Grid -> IO ()
+iterateGame first second fGrid sGrid = do
+    putStrLn "\nCurrent Grid:"
+    putStrLn $ showGrids fGrid sGrid
+
+    firstMove <- getMove first fGrid sGrid
+    let fGrid' = applyMove (firstMove - 1) fGrid
+
+    if checkWin fGrid' then
+        putStrLn $ "\n" ++ getName first ++ " wins!!"
+    else
+        if countGrid fGrid' + countGrid sGrid == 9 then
+            putStrLn $ "\nGame Over - draw"
+        else
+            iterateGame second first sGrid fGrid'
+
+            showIntro :: IO ()
+            showIntro = do
+                putStrLn "---------------------------------"
+                putStrLn "Welcome to Tic Tac Toe Haskell!!"
+                putStrLn "---------------------------------\n\n"
+
+            gameMenu :: IO ()
+            gameMenu = do
+                putStrLn "--------- Game Menu ------------"
+                choice <- getListInput ["One Player", "Two player", "Leave Game"]
+
+                case choice of
+                    1   -> do
+                        putStrLn "\n-------------- One Player Game ---------------"
+                        putStrLn "Enter player name:"
+                        name <- getStringInput
+
+                        putStrLn "Choose difficulty:"
+                        difficulty <- getListInput ["Easy"]
+
+                        let human = Player name (humanPlayer name)
+                        let computer = Player "Computer" $
+                                        case difficulty of
+                                            1   ->  easyComputer
+                                            _   -> undefined
+
+                        playGame human computer
+                        gameMenu
+
+                    2   -> do
+                        putStrLn "\n-------------- Two Player Game ---------------"
+                        putStrLn "\nEnter player one's name:"
+                        firstName <- getStringInput
+
+                        putStrLn "\nEnter player two's name:"
+                        secondName <- getStringInput
+
+                        let player1 = Player firstName (humanPlayer firstName)
+                            player2 = Player secondName (humanPlayer secondName)
+
+                        playGame player1 player2
+                        gameMenu
+
+                    _   -> putStrLn "Thanks for playing!!"
 
 main :: IO ()
 main = showIntro >> gameMenu
